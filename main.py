c731f9d50d5e6fe99e0e7fa742e3e66b4327c77b
badonyt <78484256+badonyt@users.noreply.github.com>
2022-03-04 09:51:14 +0000

    Changed name of variable and added bug fixes
    you only need now to touch once on the main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
 import random
-from settings import imagiane, nphotos, nlayers
+from settings import pathss, nphotos, nlayers
 
 
 randomizer = []
@@ -20,35 +20,41 @@ def getfiles(path):
 
 
 def mainss():
-    # "lASDosdAadasM"
-    # on pog depending on how much layers you have you will add getfiles2 3 etc
+    
     def pog():
         randomizer.clear()
-        getfiles(imagiane[0])
-        getfiles(imagiane[1])
+        for i in range(0, nlayers):
+            getfiles(pathss[i])
+        
    
 
 
     for i in range(0, nphotos):
         
         pog()
-        saveeeeed.append(randomizer[0])
-        saveeeeed.append(randomizer[1])
-        
+        # print(randomizer)
         for i in range(0, nlayers):
-            plt.imshow(plt.imread(randomizer[i]))
+            saveeeeed.append(randomizer[i])
+            # saveeeeed.append(randomizer[0])
+            # saveeeeed.append(randomizer[1])
+        
+        # "lASDosdAadasM"
+        # on the part below depending on how much layers you have you will add plt.imshow(plt.imread(randomizer[2])) 3 and so on
+        # (Tip just copy and paste for how much layers you need and change the numbers)
+        plt.imshow(plt.imread(randomizer[0]))
+        plt.imshow(plt.imread(randomizer[1]))
         plt.savefig(f'final/finalrender{i + 1}.png')
         plt.clf()
         
         
 if Stillindevelopment == True:
     print("Hello, Thank you for downloading this Project, Its still in development so its still not functional so then please wait till it works")
-elif len(imagiane) < 2 or isinstance(nphotos, int) == False or isinstance(nlayers, int) == False:
+elif len(pathss) < 2 or isinstance(nphotos, int) == False or isinstance(nlayers, int) == False:
     print("Make sure that you dont leave the none of the settings.py variables are blank. If you dont know what that does please read the settings.py")    
     print("Any questions make sure to ask on the github page or in a our discord server")
 else:
     mainss()
-    print(saveeeeed)
+    # print(saveeeeed)
 
 
 
--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # The folder where it is the images, delete the starting images and if you want to add layers just add one more, can only be a list
-imagiane = ["./photos/bc","./photos/porcima"]
+pathss = ["./photos/bc","./photos/porcima"]
 #example if you want to add one more layer = ["./photos/bc","./photos/porcima", "./photos/urmom"]
 
 #number of photos that will be produced, can only be an integer
-nphotos = 7
+nphotos = 2
 
 # number of layers, can only be an integer
 nlayers = 2
